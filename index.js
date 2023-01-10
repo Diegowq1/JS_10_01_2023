8206b89c3eee9279c429a339c24799af943b7bbb
Técnico em Informatica 2022.1 <39127512022.1@senacrjedu.local>
2023-01-10 10:52:16 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,154 @@
+// console.log("Code Runner rodando com sucesso")
+
+
+
+
+
+
+//Calcular a porcentagem de um número
+
+/*
+function porcentagem(numero, por)
+{
+    return (numero/100) * por;
+}
+
+console.log(porcentagem(200,60));
+*/
+
+
+
+
+
+
+
+//soma de dois valores
+/*
+function soma (valor1, valor2)
+{
+    return (valor1 + valor2);
+}
+
+console.log(soma(9.8, 80));
+*/
+
+
+
+
+
+
+
+
+
+/*soma, subtração, divisão e multiplicação 
+entre dois números
+
+function operacoes (valor1, valor2)
+
+{
+
+    console.log(`A soma entre ${valor1} e ${valor2} é igual a ${valor1 + valor2}`);
+
+    console.log(`A subtração entre ${valor1} e ${valor2} é igual a ${valor1 - valor2}`);
+
+    console.log(`A multiplicação entre ${valor1} e ${valor2} é igual a ${valor1 * valor2}`);
+
+    console.log(`A divisão entre ${valor1} e ${valor2} é igual a ${valor1 / valor2}`);
+    
+}
+
+operacoes(12, 4);
+*/
+
+
+
+
+
+
+
+
+
+
+//Criar função que verifica se um numero inteiro é divisivel por 3
+/*
+function divisivel (numeroint) {
+
+if (numeroint % 3 ==0) {
+
+    return true
+} else {
+    
+    return false
+}
+}
+
+console.log(divisivel(99));
+*/
+
+
+
+
+
+
+
+
+
+
+//Criar mecanismo que exiba se um usuario está logado ou não.
+/*
+autenticado = true;
+
+switch (autenticado) {
+    
+    case true:
+        console.log ("O usuário está logado");
+        break;
+
+        case false:
+            console.log("O usuário não está logado");
+
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+// Fazer uma conversão de temperatura de Celsius para Fahrenheit
+/*
+function cpf(n) {
+
+    return n * 1.8 + 32
+
+}
+
+console.log (cpf(20))
+*/
+
+
+
+
+
+
+
+
+
+
+// Fazer uma conversão de temperatura de Fahrenheit para Celsius
+
+function fpc (n) {
+
+return (n - 32) / 1.8;
+
+}
+
+console.log(fpc(68));
+
